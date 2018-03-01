6263b6aed7c5f931b311616c946bd87a710ee42e
Jai <rayeejaya@gmail.com>
2018-03-01 14:00:57 -0500

    removing node_modules

--- server/server.js
+++ server/server.js
@@ -1,64 +1,28 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require("body-parser");
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-// save new something 
-// var TodoApp = mongoose.model('Todo', {
-//     text: {
-//         type: String,
-//         require: true,
-//         minlength: 1
-//     },
-//     completed:{
-//         type: Boolean,
-//         default: false
-//     },
-//     completedAt:{
-//         type: Number,
-//         default: null
-//     }
-// });
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/users');
 
-// var newTodo = new TodoApp({
-//     text: 'cook dinner'
-// });
+var app = express();
+app.use(bodyParser.json());
 
-// newTodo.save().then((doc)=>{
-//     console.log('Save todo', doc);
-// }, (e) =>{
-//     console.log('Unable to save the todo');
-// });
-// var otherTodo = new TodoApp({
-//     text:'Edit this vedio',
-    
-// });
-// otherTodo.save().then((doc)=>{
-//     console.log(JSON.stringify(doc, undefined, 2));
-// }, (e) =>{
-//     console.log('Unable to save the todo');
-// });
-var User = mongoose.model('User', {
-    email: {
-        type: String,
-        require: true,
-        trim: true,
-        minlength: 1
-    },
-    completed:{
-        type: Boolean,
-        default: false
-    },
-    completedAt:{
-        type: Number,
-        default: null
-    }
+app.post('/todos', (req, res)=>{
+    var todo = new Todo({
+        text: req.body.text
+    });
+    todo.save().then((doc) =>{
+        res.send(doc);
+    },(e) =>{
+        res.status(400).send(e);
+    });
 });
-var user = new User({
-    email:'rayeejaya@mail.com   ' 
-});
-user.save().then((doc)=>{
-    console.log('User saved', doc)
-}, (e)=>{
-    console.log('Unable to save user', e)
+
+// GET/todos/
+
+
+app.listen(3000, () =>{
+console.log('Started on prot 3000');
 });

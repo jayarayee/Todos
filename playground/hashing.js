85f046294e3f010b80f4e7fde5d247fea0c5884d
Jai <rayeejaya@gmail.com>
2018-03-08 10:49:02 -0500

    add POST/user/login method and hashing password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,3 +1,18 @@
 const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var message 
+var password = '123abc!';
+
+bcrypt.genSalt(10, (err, salt)=>{
+    bcrypt.hash(password, salt, (err, hash)=>{
+        console.log(hash);
+        
+    });
+});
+
+var hashPassword = '$2a$10$fVl97fSUebu0d5r/WmYVFe8hYYyMB2HKWJirPZKY.50DGbEqbWMTi';
+
+bcrypt.compare(password, hashPassword, (err, res)=>{
+    console.log(res);
+});
